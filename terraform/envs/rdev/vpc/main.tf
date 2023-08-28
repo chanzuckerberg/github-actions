ffd22760628d7987df9928b6007e768e17a89d32
Jake Heath <76011913+jakeyheath@users.noreply.github.com>
2023-08-28 09:26:48 -0700

    feat: terraform plan github action (#213)
    * go
    
    * setup node
    
    * tf version
    
    * 1.3.0
    
    * dots
    
    * remove prs
    
    * repo
    
    * go
    
    * pretty
    
    * details
    
    * github sha
    
    * bracket
    
    * validate failed
    
    * continue on error
    
    * success
    
    * continue
    
    * continue
    
    * caching
    
    * validate
    
    * working dir
    
    * nudge
    
    * inputs
    
    * try again
    
    * add lock file
    
    * nudge
    
    * try again
    
    * new providers
    
    * new folder
    
    * try remote backend
    
    * backend
    
    * escape
    
    * markdown
    
    * quotes
    
    * wrong thing
    
    * go
    
    * remove capture
    
    * link
    
    * new cache location
    
    * update lockfile
    
    * try again
    
    * cache bust
    
    * news
    
    * append do not create a new one
    
    * update lockfile
    
    * bust
    
    * more platforms
    
    * another component
    
    * go
    
    * format
    
    * remove links
    
    * template
    
    * quote
    
    * shared cache
    
    * split save and restore
    
    * remove fmt
    
    * change up the cache
    
    * needs node
    
    * node
    
    * known hosts
    
    * remove module

---
+++ terraform/envs/rdev/vpc/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "remote" {
+    hostname     = "si.prod.tfe.czi.technology"
+    organization = "shared-infra"
+
+    workspaces {
+      name = "jheath-test-acction"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.56.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.6.1"
+    }
+  }
+}
+
+resource "random_id" "server" {
+  byte_length = 8
+}
