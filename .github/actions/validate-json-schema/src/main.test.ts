210a771d3dc23621d5cdff1ee27e6bd2b3c26bca
Hayden Spitzley <hspitzley@chanzuckerberg.com>
2025-03-17 11:59:18 -0600

    chore: add tests and workflow to run tests in CI

---
+++ .github/actions/validate-json-schema/src/main.test.ts
@@ -0,0 +1,52 @@
+import { when } from 'jest-when';
+import * as core from '@actions/core';
+import { main } from './main';
+
+jest.mock('@actions/core');
+const mockedCore = core as jest.Mocked<typeof core>;
+
+describe('validate-json-schema', () => {
+  it('should succeed when data matches schema [simple]', () => {
+    when(mockedCore.getInput)
+      .calledWith('data', { required: true })
+      .mockReturnValue(JSON.stringify('testvalue'));
+    when(mockedCore.getInput)
+      .calledWith('schema', { required: true })
+      .mockReturnValue(JSON.stringify({ type: 'string' }));
+
+    main();
+  });
+
+  it('should fail when data does not match schema [simple]', async () => {
+    when(mockedCore.getInput)
+      .calledWith('data', { required: true })
+      .mockReturnValue(JSON.stringify({ key: 'value' }));
+    when(mockedCore.getInput)
+      .calledWith('schema', { required: true })
+      .mockReturnValue(JSON.stringify({ type: 'string' }));
+
+    await expect(async () => await main()).rejects.toThrow('JSON is invalid: data must be string');
+  });
+
+  // it('should succeed when data matches schema [complex]', () => {
+  //   when(mockedCore.getInput)
+  //     .calledWith('data', { required: true })
+  //     .mockReturnValue(JSON.stringify('testvalue'));
+  //   when(mockedCore.getInput)
+  //     .calledWith('schema', { required: true })
+  //     .mockReturnValue(JSON.stringify({ type: 'string' }));
+
+  //   main();
+  // });
+
+  // it('should fail when data does not match schema [complex]', async () => {
+  //   when(mockedCore.getInput)
+  //     .calledWith('data', { required: true })
+  //     .mockReturnValue(JSON.stringify({ key: 'value' }));
+  //   when(mockedCore.getInput)
+  //     .calledWith('schema', { required: true })
+  //     .mockReturnValue(JSON.stringify({ type: 'string' }));
+
+  //   await expect(async () => await main()).rejects.toThrow('JSON is invalid: data must be string');
+  // });
+});
