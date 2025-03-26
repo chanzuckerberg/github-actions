691ba087bcb88f1972e5833ec3c7185312af553a
Hayden Spitzley <hspitzley@chanzuckerberg.com>
2025-03-26 11:31:04 -0600

    feat: convert argus-builder manifest-update to TS

---
+++ .github/actions/argus-builder/manifest-update/src/main.ts
@@ -0,0 +1,36 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+// eslint-disable-next-line import/no-relative-packages
+import { getCommaDelimitedArrayInput, ProcessedImage } from '../../common';
+
+type Inputs = {
+  shouldBuild: boolean
+  shouldDeploy: boolean
+  images: ProcessedImage[]
+  imageTag: string
+  buildResult: string
+  envs: string[]
+};
+
+export function getInputs(): Inputs {
+  return {
+    shouldBuild: core.getBooleanInput('should_build', { required: true }),
+    shouldDeploy: core.getBooleanInput('should_deploy', { required: true }),
+    images: JSON.parse(core.getInput('images', { required: true })),
+    imageTag: core.getInput('image_tag', { required: true }),
+    buildResult: core.getInput('build_result', { required: true }),
+    envs: getCommaDelimitedArrayInput('envs', { required: true }),
+  };
+}
+
+
+if (process.env.NODE_ENV !== 'test') {
+  main();
+}
+
+export async function main() {
+  const inputs = getInputs();
+  core.info(`Received input: ${JSON.stringify(inputs, null, 2)}`);
+
+
+}
