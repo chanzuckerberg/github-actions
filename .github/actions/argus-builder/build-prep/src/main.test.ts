367e22313ff3f180fc4f42f6182ae3fbef4f47cd
Hayden Spitzley <105455169+hspitzley-czi@users.noreply.github.com>
2025-03-31 11:27:48 -0600

    feat!: convert argus-builder build-prep action to TS (#386)

---
+++ .github/actions/argus-builder/build-prep/src/main.test.ts
@@ -0,0 +1,338 @@
+import {
+  findMatchingChangedFiles, isLabelOnPullRequest, isMatchingBranch, ProcessedImage, processImagesInput, wildcardMatch,
+} from './main';
+
+// mock core to suppress logs
+jest.mock('@actions/core');
+
+describe('validate-json-schema', () => {
+  describe('wildcardMatch', () => {
+    it('should match exact string', () => {
+      expect(wildcardMatch('test', 'test')).toBe(true);
+    });
+
+    it('should match wildcard', () => {
+      expect(wildcardMatch('test', 't*')).toBe(true);
+    });
+
+    it('should not match', () => {
+      expect(wildcardMatch('test', 't')).toBe(false);
+    });
+
+    it('should match wildcards with extra characters', () => {
+      expect(wildcardMatch('test', 't*st')).toBe(true);
+    });
+
+    it('should match multiple wildcards', () => {
+      expect(wildcardMatch('testing', 't*t*g')).toBe(true);
+    });
+
+    it('should match regex characters as literaly', () => {
+      expect(wildcardMatch('amd[64]', 'amd[64]')).toBe(true);
+      expect(wildcardMatch('amd[64]', 'amd[6*')).toBe(true);
+      expect(wildcardMatch('$mylabel', '$mylabel')).toBe(true);
+      expect(wildcardMatch('$mylabel', '$mylab*')).toBe(true);
+    });
+  });
+
+  describe('isMatchingBranch', () => {
+    it('should match exact branch', () => {
+      expect(isMatchingBranch({
+        branchesInclude: ['main'],
+        branchesIgnore: [],
+        branch: 'main',
+      })).toBe(true);
+    });
+
+    it('should match wildcard branch', () => {
+      expect(isMatchingBranch({
+        branchesInclude: ['*'],
+        branchesIgnore: [],
+        branch: 'feature/test',
+      })).toBe(true);
+    });
+
+    it('should not match wildcard branch with exclusion', () => {
+      expect(isMatchingBranch({
+        branchesInclude: ['*'],
+        branchesIgnore: ['feature/*'],
+        branch: 'feature/test',
+      })).toBe(false);
+    });
+  });
+
+  describe('findMatchingChangedFiles', () => {
+    it('should match path', () => {
+      expect(findMatchingChangedFiles(['src/main.ts'], [['src/main.ts']])).toEqual(['src/main.ts']);
+      expect(findMatchingChangedFiles(['src/main.ts'], [['src/*.ts']])).toEqual(['src/main.ts']);
+      expect(findMatchingChangedFiles(['src/main.ts'], [['**/*']])).toEqual(['src/main.ts']);
+      expect(findMatchingChangedFiles(['src/main.ts', 'src/test.ts'], [['src/*.ts']])).toEqual(['src/main.ts', 'src/test.ts']);
+      expect(
+        findMatchingChangedFiles(['lib/main.ts', 'src/test.ts'], [['lib/*.ts'], ['src/test.ts']]),
+      ).toEqual(['lib/main.ts', 'src/test.ts']);
+    });
+
+    it('should not match', () => {
+      expect(findMatchingChangedFiles(['src/main.ts', 'src/test.ts'], [['lib/*.ts']])).toEqual([]);
+      expect(findMatchingChangedFiles(['src/main.ts', 'src/test.ts'], [['src/*.ts', '!src/test.ts']])).toEqual(['src/main.ts']);
+    });
+  });
+
+  describe('isLabelOnPullRequest', () => {
+    it('should match label', () => {
+      expect(isLabelOnPullRequest(['testlabel'], ['testlabel'])).toBe(true);
+      expect(isLabelOnPullRequest(['testlabel', 'testlabel2'], ['testlabel'])).toBe(true);
+      expect(isLabelOnPullRequest(['testlabel', 'testlabel2'], ['testlabel', 'testlabel2'])).toBe(true);
+      expect(isLabelOnPullRequest(['testlabel', 'testlabel2'], ['test*'])).toBe(true);
+    });
+
+    it('should not match label', () => {
+      expect(isLabelOnPullRequest([], ['testlabel'])).toBe(false);
+      expect(isLabelOnPullRequest(['testlabel'], ['testlabel2'])).toBe(false);
+      expect(isLabelOnPullRequest(['testlabel', 'testlabel2'], ['testlabel3'])).toBe(false);
+    });
+  });
+
+  describe('processImagesInput', () => {
+    it('processes platform, build_args, secret_files, and argus_root', () => {
+      const processedImage: ProcessedImage[] = processImagesInput(
+        {
+          frontend: {
+            context: 'frontend',
+            dockerfile: 'frontend/Dockerfile',
+            build_args: ['ARG1=VALUE1', 'ARG2=VALUE2'],
+            secret_files: ['secret.txt', 'secret2.txt'],
+            argus_root: 'frontend',
+          },
+        },
+        ['src/main.ts'],
+        'feature-123',
+      );
+
+      expect(processedImage).toEqual([{
+        name: 'frontend',
+        context: 'frontend',
+        dockerfile: 'frontend/Dockerfile',
+        platform: 'linux/arm64',
+        build_args: 'ARG1=VALUE1\nARG2=VALUE2',
+        secret_files: 'secret.txt\nsecret2.txt',
+        argus_root: 'frontend',
+        files_matched: true,
+        branch_matched: true,
+        should_build: true,
+      }]);
+    });
+
+    it('is expected to build with default branch and file filters', () => {
+      const processedImage: ProcessedImage[] = processImagesInput(
+        {
+          frontend: {
+            context: 'frontend',
+            dockerfile: 'frontend/Dockerfile',
+          },
+        },
+        ['src/main.ts'],
+        'feature-123',
+      );
+
+      expect(processedImage).toEqual([{
+        name: 'frontend',
+        context: 'frontend',
+        dockerfile: 'frontend/Dockerfile',
+        platform: 'linux/arm64',
+        build_args: '',
+        secret_files: '',
+        argus_root: '.',
+        files_matched: true,
+        branch_matched: true,
+        should_build: true,
+      }]);
+    });
+
+    it('is NOT expected to build with default branch and file filters when no files changed', () => {
+      const processedImage: ProcessedImage[] = processImagesInput(
+        {
+          frontend: {
+            context: 'frontend',
+            dockerfile: 'frontend/Dockerfile',
+          },
+        },
+        [],
+        'feature-123',
+      );
+
+      expect(processedImage).toEqual([{
+        name: 'frontend',
+        context: 'frontend',
+        dockerfile: 'frontend/Dockerfile',
+        platform: 'linux/arm64',
+        build_args: '',
+        secret_files: '',
+        argus_root: '.',
+        files_matched: false,
+        branch_matched: true,
+        should_build: false,
+      }]);
+    });
+
+    it('is NOT expected to build when branch does not match', () => {
+      const processedImage: ProcessedImage[] = processImagesInput(
+        {
+          frontend: {
+            context: 'frontend',
+            dockerfile: 'frontend/Dockerfile',
+            branches_include: ['main'],
+          },
+        },
+        ['src/main.ts'],
+        'feature-123',
+      );
+
+      expect(processedImage).toEqual([{
+        name: 'frontend',
+        context: 'frontend',
+        dockerfile: 'frontend/Dockerfile',
+        platform: 'linux/arm64',
+        build_args: '',
+        secret_files: '',
+        argus_root: '.',
+        files_matched: true,
+        branch_matched: false,
+        should_build: false,
+      }]);
+    });
+
+    it('is NOT expected to build when branch is excluded', () => {
+      const processedImage: ProcessedImage[] = processImagesInput(
+        {
+          frontend: {
+            context: 'frontend',
+            dockerfile: 'frontend/Dockerfile',
+            branches_ignore: ['feature-*'],
+          },
+        },
+        ['src/main.ts'],
+        'feature-123',
+      );
+
+      expect(processedImage).toEqual([{
+        name: 'frontend',
+        context: 'frontend',
+        dockerfile: 'frontend/Dockerfile',
+        platform: 'linux/arm64',
+        build_args: '',
+        secret_files: '',
+        argus_root: '.',
+        files_matched: true,
+        branch_matched: false,
+        should_build: false,
+      }]);
+    });
+
+    it('is expected to build when branch is included', () => {
+      const processedImage: ProcessedImage[] = processImagesInput(
+        {
+          frontend: {
+            context: 'frontend',
+            dockerfile: 'frontend/Dockerfile',
+            branches_include: ['feature-*'],
+          },
+        },
+        ['src/main.ts'],
+        'feature-123',
+      );
+
+      expect(processedImage).toEqual([{
+        name: 'frontend',
+        context: 'frontend',
+        dockerfile: 'frontend/Dockerfile',
+        platform: 'linux/arm64',
+        build_args: '',
+        secret_files: '',
+        argus_root: '.',
+        files_matched: true,
+        branch_matched: true,
+        should_build: true,
+      }]);
+    });
+
+    it('is expected to build when changed files match path filters', () => {
+      const processedImage: ProcessedImage[] = processImagesInput(
+        {
+          frontend: {
+            context: 'frontend',
+            dockerfile: 'frontend/Dockerfile',
+            path_filters: ['src/*.ts'],
+          },
+        },
+        ['src/main.ts'],
+        'feature-123',
+      );
+
+      expect(processedImage).toEqual([{
+        name: 'frontend',
+        context: 'frontend',
+        dockerfile: 'frontend/Dockerfile',
+        platform: 'linux/arm64',
+        build_args: '',
+        secret_files: '',
+        argus_root: '.',
+        files_matched: true,
+        branch_matched: true,
+        should_build: true,
+      }]);
+    });
+
+    it('is NOT expected to build when changed files do not match path filters', () => {
+      const processedImage: ProcessedImage[] = processImagesInput(
+        {
+          frontend: {
+            context: 'frontend',
+            dockerfile: 'frontend/Dockerfile',
+            path_filters: ['lib/*.ts'],
+          },
+        },
+        ['src/main.ts'],
+        'feature-123',
+      );
+
+      expect(processedImage).toEqual([{
+        name: 'frontend',
+        context: 'frontend',
+        dockerfile: 'frontend/Dockerfile',
+        platform: 'linux/arm64',
+        build_args: '',
+        secret_files: '',
+        argus_root: '.',
+        files_matched: false,
+        branch_matched: true,
+        should_build: false,
+      }]);
+
+      const processedImage2: ProcessedImage[] = processImagesInput(
+        {
+          frontend: {
+            context: 'frontend',
+            dockerfile: 'frontend/Dockerfile',
+            path_filters: [['src/**', '!src/main.ts']],
+          },
+        },
+        ['src/main.ts'],
+        'feature-123',
+      );
+
+      expect(processedImage2).toEqual([{
+        name: 'frontend',
+        context: 'frontend',
+        dockerfile: 'frontend/Dockerfile',
+        platform: 'linux/arm64',
+        build_args: '',
+        secret_files: '',
+        argus_root: '.',
+        files_matched: false,
+        branch_matched: true,
+        should_build: false,
+      }]);
+    });
+  });
+});
---
+++ .github/actions/find-changed-files/src/findChangedFiles.ts
@@ -0,0 +1,71 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { GitHub } from '@actions/github/lib/utils';
+
+type Output = {
+  allModifiedFiles: string[]
+};
+
+export async function findChangedFiles(githubToken: string): Promise<Output> {
+  const gitClient = github.getOctokit(githubToken);
+
+  const { owner, repo } = github.context.repo;
+
+  if (github.context.eventName === 'pull_request') {
+    const prNumber = github.context.payload.pull_request?.number;
+    if (!prNumber) {
+      throw new Error('No pull request number found');
+    }
+    const changedFilePaths = await getChangedFilesInPR(gitClient, repo, owner, prNumber);
+    core.info(`Triggered by pull_request event, found changed files in PR: ${JSON.stringify(changedFilePaths, null, 2)}`);
+    return { allModifiedFiles: changedFilePaths };
+  } if (github.context.eventName === 'push') {
+    core.debug(`Push event detected ${JSON.stringify(github.context.payload, null, 2)}`);
+    const commitSha = github.context.sha;
+
+    const isForcedPush = github.context.payload.forced || false;
+    if (isForcedPush) {
+      const result = await gitClient.rest.repos.listPullRequestsAssociatedWithCommit({
+        owner,
+        repo,
+        commit_sha: commitSha,
+      });
+
+      const openPRs = result.data.filter((pr) => pr.state === 'open');
+      if (openPRs.length === 0) {
+        return { allModifiedFiles: [] };
+      }
+
+      const pr = openPRs[0];
+      const changedFilePaths = await getChangedFilesInPR(gitClient, repo, owner, pr.number);
+      core.info(`Triggered by push (force=true), found changed files in PR: ${JSON.stringify(changedFilePaths, null, 2)}`);
+      return { allModifiedFiles: changedFilePaths };
+    }
+
+    const commitResp = await gitClient.rest.repos.getCommit({
+      owner,
+      repo,
+      ref: commitSha,
+    });
+
+    const changedFilePaths = (commitResp.data.files || []).map((file) => file.filename);
+    core.info(`Triggered by push (force=false), found changed files in commit: ${JSON.stringify(changedFilePaths, null, 2)}`);
+    return { allModifiedFiles: changedFilePaths };
+  }
+  throw new Error(`EventName ${github.context.eventName} not supported`);
+}
+
+async function getChangedFilesInPR(
+  gitClient: InstanceType<typeof GitHub>,
+  repo: string,
+  owner: string,
+  prNumber: number,
+): Promise<string[]> {
+  const listFilesResp = await gitClient.rest.pulls.listFiles({
+    owner,
+    repo,
+    pull_number: prNumber,
+  });
+
+  return listFilesResp.data.map((file) => file.filename);
+}
